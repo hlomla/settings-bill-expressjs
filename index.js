a5439986f10f7923b33ca788faf321e5891a81b3
hlomla <hlomla.tapuko@gmail.com>
2021-08-04 10:30:35 +0200

    working on functionality

---
+++ index.js
@@ -0,0 +1,54 @@
+let express = require('express')
+const exphbs  = require('express-handlebars');
+
+// const SettingsBill = require('./settingsBill');
+const handlebarSetup = exphbs({
+    partialsDir: "./views/partials",
+    viewPath: './views',
+    layoutsDir: './views/layouts'
+    }); 
+
+const settingsBill = require('./settingsBill');
+
+let app =  express()
+const SettingsBILL = settingsBill()
+let PORT = process.env.PORT || 3011;
+
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.render('index', {settings: SettingsBILL.getSettings(), 
+        })
+});
+
+app.post('/settings', function(req, res){
+
+    SettingsBILL.setSettings({
+
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    });
+    console.log(SettingsBILL.getSettings)
+    res.redirect('/')
+});
+
+app.post('/action', function(req, res){
+    
+});
+
+app.get('/actions', function(req, res){
+    
+});
+
+app.get('/actions/:type', function(req, res){
+    
+});
+
+app.listen(PORT, function(){
+    console.log("App started at PORT: ", PORT);
+})
